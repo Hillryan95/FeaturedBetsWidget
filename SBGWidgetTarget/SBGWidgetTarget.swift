dbb5909f2ff3a88f30e54f2122a504d7ee671754
Ryan Hill <ryan.hill@skybettingandgaming.com>
2022-04-08 12:19:21 +0100

    Widget Target Configured

--- SBGWidgetTarget/SBGWidgetTarget.swift
+++ SBGWidgetTarget/SBGWidgetTarget.swift
@@ -8,27 +8,35 @@
 import WidgetKit
 import SwiftUI
 import Intents
+//import SBGWidget
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(date: Date(), name: "", price: "", outcome: "", sport: "", location: "", time: "", configuration: ConfigurationIntent())
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        let entry = SimpleEntry(date: Date(), name: "", price: "", outcome: "", sport: "", location: "", time: "", configuration: configuration)
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
 
+        let userDefaults = UserDefaults(suiteName: "group.RyanHill.SBGWidget")
+        guard let userDefaults = userDefaults else { return }
+
+        guard let featuredBetArray = userDefaults.object(forKey: "featuredBet") as? [String] else { return }
+        let name = featuredBetArray[0]
+        let price = featuredBetArray[1]
+        let outcome = featuredBetArray[2]
+        let sport = featuredBetArray[3]
+        let location = featuredBetArray[4]
+        let time = featuredBetArray[5]
+
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
+        let entry = SimpleEntry(date: Date(), name: name , price: price, outcome: outcome, sport: sport, location: location, time: time, configuration: configuration)
+        entries.append(entry)
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
@@ -36,7 +44,14 @@ struct Provider: IntentTimelineProvider {
 }
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
+    var date: Date
+
+    let name: String
+    let price: String
+    let outcome: String
+    let sport: String
+    let location: String
+    let time: String
     let configuration: ConfigurationIntent
 }
 
@@ -44,7 +59,12 @@ struct SBGWidgetTargetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        Text(entry.date, style: .time)
+        ZStack {
+            Color(UIColor.skyBetBlue!)
+
+            FeaturedBetCardView(name: entry.name, price: entry.price, outcome: entry.outcome, sport: entry.sport, location: entry.location, time: entry.time)
+                .widgetURL(URL(string: "sbgwidget://betslip")!)
+        }
     }
 }
 
@@ -60,10 +80,10 @@ struct SBGWidgetTarget: Widget {
         .description("This is an example widget.")
     }
 }
-
-struct SBGWidgetTarget_Previews: PreviewProvider {
-    static var previews: some View {
-        SBGWidgetTargetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-            .previewContext(WidgetPreviewContext(family: .systemSmall))
-    }
-}
+//
+//struct SBGWidgetTarget_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SBGWidgetTargetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+//            .previewContext(WidgetPreviewContext(family: .systemSmall))
+//    }
+//}
