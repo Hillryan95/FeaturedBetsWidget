736723dc7622aed3601cd902dff26ed5def95caa
Ryan Hill <ryan.hill@skybettingandgaming.com>
2022-04-08 12:20:30 +0100

    Mock Betslip View Controller added

--- SBGWidget/Services/EncodeFeaturedBetToUserDefaults.swift
+++ SBGWidget/Services/EncodeFeaturedBetToUserDefaults.swift
@@ -5,4 +5,22 @@
 //  Created by Ryan Hill on 03/03/2022.
 //
 
-import Foundation
+import SwiftUI
+import WidgetKit
+
+struct EncodeFeaturedBetToUserDefaults {
+
+    @AppStorage("rememberItem", store: UserDefaults(suiteName: "group.RyanHill.SBGWidget")) var featuredBetData: Data = Data()
+    let featuredBet: FeaturedBet
+
+    func storeBet() {
+        let encoder = JSONEncoder()
+        guard let data = try? encoder.encode(featuredBet) else {
+            print("Could not encode data")
+            return
+        }
+        featuredBetData = data
+        WidgetCenter.shared.reloadAllTimelines()
+        print(String(decoding: featuredBetData, as: UTF8.self))
+    }
+}
